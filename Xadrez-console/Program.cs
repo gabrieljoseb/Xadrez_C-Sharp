070e35b5036fc0ed9a48e9cecbc492cc6679aa4f
Gabriel Ribeiro <gjose.benevides@gmail.com>
2020-03-25 09:34:41 -0300

    Melhorando implementacao do metodo ImprimirTabuleiro, com diferenciacao de cores entre as pecas

--- Xadrez-console/Program.cs
+++ Xadrez-console/Program.cs
@@ -12,12 +12,16 @@ namespace Xadrez_console
             {
                 Tabuleiro tab = new Tabuleiro(8, 8);
                 tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(1, 3));
+                tab.ColocarPeca(new Rei(tab, Cor.Branca), new Posicao(1, 3));
+                tab.ColocarPeca(new Rei(tab, Cor.Branca), new Posicao(1, 5));
                 tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(4, 5));
+                tab.ColocarPeca(new Rei(tab, Cor.Branca), new Posicao(4, 7));
+                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 5));
                 Tela.ImprimirTabuleiro(tab);
 
-                PosicaoXadrez posX = new PosicaoXadrez('B', 5);
+                PosicaoXadrez posX = new PosicaoXadrez('a', 8);
                 Console.WriteLine(posX);
+                Console.WriteLine(posX.ToPosicao());
 
             }
             catch (DomainException e)
