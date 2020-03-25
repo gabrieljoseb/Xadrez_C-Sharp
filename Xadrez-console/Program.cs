cf91b7ca382be6f7092b6642b911320be60edc8f
Gabriel Ribeiro <gjose.benevides@gmail.com>
2020-03-25 09:12:10 -0300

    Criada a classe Posicao Xadrez

--- Xadrez-console/Program.cs
+++ Xadrez-console/Program.cs
@@ -11,12 +11,14 @@ namespace Xadrez_console
             try
             {
                 Tabuleiro tab = new Tabuleiro(8, 8);
-
                 tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
                 tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(1, 3));
                 tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(4, 5));
-
                 Tela.ImprimirTabuleiro(tab);
+
+                PosicaoXadrez posX = new PosicaoXadrez('B', 5);
+                Console.WriteLine(posX);
+
             }
             catch (DomainException e)
             {
