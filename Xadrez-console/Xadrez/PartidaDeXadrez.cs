be74702005ee985ec3e0575040b72bd2d3d19c66
Gabriel Ribeiro <gjose.benevides@gmail.com>
2020-03-27 13:05:24 -0300

    Renomeando o método para MovimentoPossivel

--- Xadrez-console/Xadrez/PartidaDeXadrez.cs
+++ Xadrez-console/Xadrez/PartidaDeXadrez.cs
@@ -86,7 +86,7 @@ namespace xadrez
 
         public void ValidarPosicaoDeDestino(Posicao origem, Posicao destino)
         {
-            if (!Tab.Peca(origem).PodeMoverPara(destino))            
+            if (!Tab.Peca(origem).MovimentoPossivel(destino))            
                 throw new DomainException("A posição de destino é inválida!");            
         }
 
