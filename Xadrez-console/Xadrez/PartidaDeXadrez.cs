da30d47d66b60c66e232eba321beed6b7cd69418
Gabriel Ribeiro <gjose.benevides@gmail.com>
2020-03-30 12:46:16 -0300

    Jogada especial promocao

--- Xadrez-console/Xadrez/PartidaDeXadrez.cs
+++ Xadrez-console/Xadrez/PartidaDeXadrez.cs
@@ -133,6 +133,20 @@ namespace xadrez
                 throw new DomainException("Você não pode se colocar em xeque!");
             }
 
+            Peca p = Tab.Peca(destino);
+
+            // #Jogada especial promocao
+            if(p is Peao)
+            {
+                if((p.Cor == Cor.Branca && destino.Linha == 0) || (p.Cor == Cor.Preta && destino.Linha == 7))
+                {
+                    p = Tab.RetirarPeca(destino);
+                    Pecas.Remove(p);
+                    Peca dama = new Dama(Tab, p.Cor);
+                    Tab.ColocarPeca(dama, destino);
+                    Pecas.Add(dama);
+                }
+            }
             if (EstaEmXeque(Adversaria(JogadorAtual)))
                 Xeque = true;
             else
@@ -146,8 +160,6 @@ namespace xadrez
                 Turno++;
                 MudaJogador();
             }
-
-            Peca p = Tab.Peca(destino);
 
             // #Jogada especial En Passant
             if (p is Peao && (destino.Linha == origem.Linha - 2 || destino.Linha == origem.Linha + 2))
